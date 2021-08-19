63bf7d61835799cb431af9e748e26af3baa6c6aa
Gabriel Pimentel <gabriel97pimentel@gmail.com>
2021-08-19 16:51:08 -0300

    creating a class to imitate the 'new' command from dotnet CLI

---
+++ Primeiros Passos .NET/CLI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+
+namespace CLI
+{
+    
+    class New
+    {
+
+        static void Main(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory();
+            Console.WriteLine("Your current directory is {0}", path);
+        }
+    }
+}
