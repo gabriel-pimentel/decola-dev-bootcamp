437f2319819c37191b46bab13ef1e7a04b20e8f1
Gabriel Pimentel <gabriel97pimentel@gmail.com>
2021-08-30 22:55:23 -0300

    Creating cadastro-serie-app

---
+++ cadastro-series-app/Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace cadastro_series_app
+{
+    public abstract class EntidadeBase
+    {
+        public int Id {get, protected set;}
+    }
+}
---
+++ cadastro-series-app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cadastro_series_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
