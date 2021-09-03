fa6fde07580ae39d988967f1e6981c10ba612982
Gabriel Pimentel <gabriel97pimentel@gmail.com>
2021-09-03 12:31:40 -0300

    fixing method after failure in test

--- c#-testes-unitarios/Bank/Bank/BankAccount.cs
+++ c#-testes-unitarios/Bank/Bank/BankAccount.cs
@@ -1,12 +1,63 @@
 ﻿using System;
 
-namespace Bank
+namespace BankAccountNS
 {
-    class Program
+    
+    public class BankAccount
     {
-        static void Main(string[] args)
+        private readonly string customerName;
+        private double balance;
+
+        private BankAccount() { }
+
+        public BankAccount(string customerName, double balance)
         {
-            Console.WriteLine("Hello World!");
+            this.customerName = customerName;
+            this.balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return this.customerName; }
+        }
+
+        public double Balance
+        {
+            get { return this.balance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (amount > this.balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            this.balance -= amount; 
+        }
+
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            this.balance += amount;
+        }
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
         }
     }
 }
--- c#-testes-unitarios/Bank/BankTests/BankAccountTests.cs
+++ c#-testes-unitarios/Bank/BankTests/BankAccountTests.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
 
 namespace BankTests
 {
@@ -7,8 +7,20 @@ namespace BankTests
     public class BankAccountTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Debit_WithValidAmount_UpdatesBalance()
         {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bean", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
         }
     }
 }
